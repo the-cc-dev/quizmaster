a31a21ee2e696f3bdda0ddee6b805bc3b52bc6af
goldhat <joel@goldhat.ca>
2017-03-14 05:10:39 -0500

    cleaning up screen handling

--- lib/controller/QuizMaster_Controller_Admin.php
+++ lib/controller/QuizMaster_Controller_Admin.php
@@ -1,15 +1,16 @@
 <?php
 
-class QuizMaster_Controller_Admin
-{
+class QuizMaster_Controller_Admin {
 
     protected $_ajax;
 
     public function __construct() {
 
+      // init ajax handlers
       $this->_ajax = new QuizMaster_Controller_Ajax();
       $this->_ajax->init();
 
+      // register the admin menu
       add_action('admin_menu', array($this, 'register_page'));
 
       // init controller email
@@ -17,42 +18,39 @@ class QuizMaster_Controller_Admin
 
     }
 
-    private function localizeScript()
-    {
+    private function localizeScript() {
         global $wp_locale;
 
         $isRtl = isset($wp_locale->is_rtl) ? $wp_locale->is_rtl : false;
 
         $translation_array = array(
-            'delete_msg' => __('Do you really want to delete the quiz/question?', 'quizmaster'),
-            'no_title_msg' => __('Title is not filled!', 'quizmaster'),
-            'no_question_msg' => __('No question deposited!', 'quizmaster'),
-            'no_correct_msg' => __('Correct answer was not selected!', 'quizmaster'),
-            'no_answer_msg' => __('No answer deposited!', 'quizmaster'),
-            'no_quiz_start_msg' => __('No quiz description filled!', 'quizmaster'),
-            'fail_grade_result' => __('The percent values in result text are incorrect.', 'quizmaster'),
-            'no_nummber_points' => __('No number in the field "Points" or less than 1', 'quizmaster'),
-            'no_nummber_points_new' => __('No number in the field "Points" or less than 0', 'quizmaster'),
-            'no_selected_quiz' => __('No quiz selected', 'quizmaster'),
-            'no_data_available' => __('No data available', 'quizmaster'),
-            'no_sort_element_criterion' => __('No sort element in the criterion', 'quizmaster'),
-            'dif_points' => __('"Different points for every answer" is not possible at "Free" choice', 'quizmaster'),
-            'category_no_name' => __('You must specify a name.', 'quizmaster'),
-            'confirm_delete_entry' => __('This entry should really be deleted?', 'quizmaster'),
-            'not_all_fields_completed' => __('Not all fields completed.', 'quizmaster'),
-            'temploate_no_name' => __('You must specify a template name.', 'quizmaster'),
-            'closeText' => __('Close', 'quizmaster'),
-            'currentText' => __('Today', 'quizmaster'),
-            'monthNames' => array_values($wp_locale->month),
-            'monthNamesShort' => array_values($wp_locale->month_abbrev),
-            'dayNames' => array_values($wp_locale->weekday),
-            'dayNamesShort' => array_values($wp_locale->weekday_abbrev),
-            'dayNamesMin' => array_values($wp_locale->weekday_initial),
-//			'dateFormat'        => QuizMaster_Helper_Until::convertPHPDateFormatToJS(get_option('date_format', 'm/d/Y')),
-            //e.g. "9 de setembro de 2014" -> change to "hard" dateformat
-            'dateFormat' => 'mm/dd/yy',
-            'firstDay' => get_option('start_of_week'),
-            'isRTL' => $isRtl
+          'delete_msg' => __('Do you really want to delete the quiz/question?', 'quizmaster'),
+          'no_title_msg' => __('Title is not filled!', 'quizmaster'),
+          'no_question_msg' => __('No question deposited!', 'quizmaster'),
+          'no_correct_msg' => __('Correct answer was not selected!', 'quizmaster'),
+          'no_answer_msg' => __('No answer deposited!', 'quizmaster'),
+          'no_quiz_start_msg' => __('No quiz description filled!', 'quizmaster'),
+          'fail_grade_result' => __('The percent values in result text are incorrect.', 'quizmaster'),
+          'no_nummber_points' => __('No number in the field "Points" or less than 1', 'quizmaster'),
+          'no_nummber_points_new' => __('No number in the field "Points" or less than 0', 'quizmaster'),
+          'no_selected_quiz' => __('No quiz selected', 'quizmaster'),
+          'no_data_available' => __('No data available', 'quizmaster'),
+          'no_sort_element_criterion' => __('No sort element in the criterion', 'quizmaster'),
+          'dif_points' => __('"Different points for every answer" is not possible at "Free" choice', 'quizmaster'),
+          'category_no_name' => __('You must specify a name.', 'quizmaster'),
+          'confirm_delete_entry' => __('This entry should really be deleted?', 'quizmaster'),
+          'not_all_fields_completed' => __('Not all fields completed.', 'quizmaster'),
+          'temploate_no_name' => __('You must specify a template name.', 'quizmaster'),
+          'closeText' => __('Close', 'quizmaster'),
+          'currentText' => __('Today', 'quizmaster'),
+          'monthNames' => array_values($wp_locale->month),
+          'monthNamesShort' => array_values($wp_locale->month_abbrev),
+          'dayNames' => array_values($wp_locale->weekday),
+          'dayNamesShort' => array_values($wp_locale->weekday_abbrev),
+          'dayNamesMin' => array_values($wp_locale->weekday_initial),
+          'dateFormat' => 'mm/dd/yy',
+          'firstDay' => get_option('start_of_week'),
+          'isRTL' => $isRtl
         );
 
         wp_localize_script('quizMaster_admin_javascript', 'quizMasterLocalize', $translation_array);
@@ -108,21 +106,20 @@ class QuizMaster_Controller_Admin
         }
     }
 
-    public function routeLoadAction()
-    {
+    public function routeLoadAction() {
+
+        // screen handling
         $screen = get_current_screen();
-
         if (!empty($screen)) {
-            // Workaround for wp_ajax_hidden_columns() with sanitize_key()
-            $name = strtolower($screen->id);
+          // Workaround for wp_ajax_hidden_columns() with sanitize_key()
+          $name = strtolower($screen->id);
 
-            if (!empty($_GET['module'])) {
-                $name .= '_' . strtolower($_GET['module']);
-            }
+          if (!empty($_GET['module'])) {
+              $name .= '_' . strtolower($_GET['module']);
+          }
 
-            set_current_screen($name);
-
-            $screen = get_current_screen();
+          set_current_screen($name);
+          $screen = get_current_screen();
         }
 
         $helperView = new QuizMaster_View_GlobalHelperTabs();
