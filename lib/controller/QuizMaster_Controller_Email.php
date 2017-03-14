42622f8e3219206d41a6df423ba6d78faf08c6ac
goldhat <joel@goldhat.ca>
2017-03-14 05:57:39 -0500

    cleanup, comments for emailExists

--- lib/controller/QuizMaster_Controller_Email.php
+++ lib/controller/QuizMaster_Controller_Email.php
@@ -57,8 +57,10 @@ class QuizMaster_Controller_Email {
   }
 
   public function addEmailTriggers() {
+
     $defaultTriggers = $this->getDefaultEmailTriggers();
     $registeredTriggers = apply_filters('quizmaster_email_triggers', $defaultTriggers);
+
     foreach( $registeredTriggers as $trigger => $label ) {
       $triggerKey = quizmaster_simplify_key( $trigger );
       $triggerKey = quizmaster_camelize( $triggerKey );
@@ -66,6 +68,7 @@ class QuizMaster_Controller_Email {
       $callback = apply_filters('quizmaster_email_trigger_callback', $callback, $trigger );
       add_action( $trigger, $callback, 10, 2);
     }
+
   }
 
   public function loadEmailTriggerList( $field ) {
@@ -78,25 +81,30 @@ class QuizMaster_Controller_Email {
   }
 
   private function getDefaultEmailTriggers() {
+
     return array(
       'quizmaster_completed_quiz' => 'Quiz Completion',
       'quizmaster_completed_quiz_100_percent' => 'Quiz Completion with 100% Score',
     );
+
   }
 
   public function send() {
+
     wp_mail(
       $this->email->getRecipients(),
       $this->email->getSubject(),
       $this->email->getMessage(),
       $this->email->getHeaders()
     );
-    print "Email Sent";
+
   }
 
   public function setMessage() {
+
     $msg = $this->parseTemplate();
     $this->email->setMessage( $msg );
+
   }
 
   public function parseTemplate() {
@@ -213,11 +221,17 @@ class QuizMaster_Controller_Email {
     return $posts;
   }
 
+  /**
+   * Check if an email post exists for the given email key
+   * @param  string $key Email key as defined in email settings
+   * @return boolean true if the email exist, or false if it does not
+   */
   public function emailExists( $key ) {
     $emails = $this->getEmailsByKey( $key );
     if( !empty( $emails )) {
       return true;
     }
+    return false;
   }
 
 }
