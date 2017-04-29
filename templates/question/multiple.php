ad4aa175edb797989b2180e116871f642891abfb
goldhat <joel@goldhat.ca>
2017-04-29 10:28:27 -0500

    fixed answers for multiple choice

--- templates/question/multiple.php
+++ templates/question/multiple.php
@@ -16,24 +16,27 @@
 
     <ul class="quizMaster_questionList" data-question_id="<?php echo $question->getId(); ?>"
       data-type="<?php echo $question->getAnswerType(); ?>">
+
       <?php
       $answer_index = 0;
 
-    foreach ($question->getAnswerData() as $v) :
-        $answer_text = $v->isHtml() ? $v->getAnswer() : esc_html($v->getAnswer());
-        ?>
+      foreach ($question->getAnswerData() as $v) :
+          $answer_text = $v->isHtml() ? $v->getAnswer() : esc_html($v->getAnswer());
+          ?>
 
-        <li class="quizMaster_questionListItem" data-pos="<?php echo $answer_index; ?>">
-          <span></span>
-          <label>
-              <input class="quizMaster_questionInput"
-                 type="checkbox"
-                 name="question_<?php echo $question->getId(); ?>"
-                 value="<?php echo($answer_index + 1); ?>"> <?php echo $answer_text; ?>
-          </label>
-        </li>
+          <li class="quizMaster_questionListItem" data-pos="<?php echo $answer_index; ?>">
+            <span></span>
+            <label>
+                <input class="quizMaster_questionInput"
+                   type="checkbox"
+                   name="question_<?php echo $question->getId(); ?>"
+                   value="<?php echo($answer_index + 1); ?>"> <?php echo $answer_text; ?>
+            </label>
+          </li>
 
-    <?php endforeach; ?>
+    <?php $answer_index++; endforeach; ?>
+
+  </ul>
 
     </div>
 </div>
