cfcb278689c9ffb6fbd588033e90ab803bb21cdf
goldhat <joel@goldhat.ca>
2017-06-05 23:54:01 -0500

    updated readme, version number

--- quizmaster.php
+++ quizmaster.php
@@ -3,7 +3,7 @@
 Plugin Name: QuizMaster
 Plugin URI: http://wordpress.org/extend/plugins/quizmaster
 Description: Best free quiz plugin for WordPress.
-Version: 0.6.0
+Version: 0.6.1
 Author: GoldHat Group
 Author URI: https://goldhat.ca
 Copyright: GoldHat Group, Julius Fischer (WP Pro Quiz)
@@ -11,7 +11,7 @@ Text Domain: quizmaster
 Domain Path: /languages
 */
 
-define('QUIZMASTER_VERSION', '0.6.0');
+define('QUIZMASTER_VERSION', '0.6.1');
 define('QUIZMASTER_DEV', true);
 define('QUIZMASTER_PATH', dirname(__FILE__));
 define('QUIZMASTER_URL', plugins_url('', __FILE__));
